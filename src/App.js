66fcbdd690e042364f894c7d8fa19b766736fb6a
Timothy Baumgartner <timmey.g.b@gmail.com>
2018-10-10 00:39:36 -0700

    Begin creating home page. Install Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>Timothy Baumgartner</h1>
+          <i>Software Developer</i>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <div className="row" id="headerRow">
+            <h3 className="projects__header">Selected Projects</h3>
+            <a href="/about" className="menu__link">About</a>
+          </div>
+          <div className="row">
+            <div className="project">
+              <h2>
+                <a href="/detail/spokane-transit-app">
+                  <span className="project__title">Unofficial Spokane Transit Authority Companion App</span>
+                </a>
+              </h2>
+              <p>
+                A work in progress to compete with <a href="https://spokanetransit.com">spokanetransit.com</a>.<br />
+                You can follow along on <a href="https://github.com/cstaaben/transit-webApp">GitHub</a> or <a href="http://timothy-b.com/transit-webApp/busForm.html">see it live</a>.<br />
+                Last updated: Nov 9
+              </p>
+            </div>
+          </div>
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
