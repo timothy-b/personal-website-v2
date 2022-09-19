fb248b67d565cbda86b52e9c440356274e4aa3df
Timothy Baumgartner <timmey.g.b@gmail.com>
2022-09-18 23:39:44 -0700

    Fix caching issue.

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 PATH=$PATH:/home/tbaumgar/bin
 /home/tbaumgar/bin/npm install -g serve
 cd /home/tbaumgar/public_html/personal-website-v2
-/usr/bin/nohup /home/tbaumgar/bin/serve -s build >> /home/tbaumgar/log.txt 2>> /home/tbaumgar/err.log
+/usr/bin/nohup /home/tbaumgar/bin/serve -n -p 5000 -s build >> /home/tbaumgar/log.txt 2>> /home/tbaumgar/err.log
